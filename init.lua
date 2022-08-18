c8227427d8a195b4544b03ac521b0271063dac9e
sljeff <kindjeff.com@gmail.com>
2022-08-18 10:29:35 +0800

    fix: nvim cmdline

--- init.lua
+++ init.lua
@@ -134,12 +134,14 @@ cmp.setup({
 })
 -- Use buffer source for `/`.
 cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = 'buffer' }
   }
 })
 -- Use cmdline & path source for ':'.
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' }
   }, {
