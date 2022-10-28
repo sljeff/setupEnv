8ac3bbf112716f8f5634d9bad73281e67ced21f3
sljeff <kindjeff.com@gmail.com>
2022-10-28 09:36:29 +0800

    feat: rm noice

--- init.lua
+++ init.lua
@@ -63,18 +63,6 @@ require('packer').startup(function()
 
   use 'github/copilot.vim'
 
-  use({
-    "folke/noice.nvim",
-    event = "VimEnter",
-    config = function()
-      require("noice").setup()
-    end,
-    requires = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-      }
-  })
 end)
 
 local lines = vim.opt.lines._value
